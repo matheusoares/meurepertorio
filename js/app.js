0b97db40af539a49918b845d04bcafdcb607039c
Matheus de Sousa <matheus_desousa@live.com>
2018-04-02 11:37:26 -0300

    Adicionando pag usuario

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
+//verifica campos de senha iguais
+function confirmaSenhaFunct(){
+	var senha = document.getElementById("inputPassword").value;
+	var confirmaSenha = document.getElementById("inputConfirmationPassword").value;
 
+	var inputSenha = document.getElementById("inputPassword");
+	var inputConfirma = document.getElementById("inputConfirmationPassword");
 
+	if(senha != false && senha == confirmaSenha){
+
+		inputConfirma.classList.add("is-valid");
+		inputSenha.classList.add("is-valid");
+		inputConfirma.classList.remove("is-invalid");
+		inputSenha.classList.remove("is-invalid");
+	}else{
+
+		inputConfirma.classList.add("is-invalid");
+		inputSenha.classList.add("is-invalid");
+		inputConfirma.classList.remove("is-valid");
+		inputSenha.classList.remove("is-valid");
+	}	
+}
+
+//termos de contrato checkbox
 var botao = document.getElementById("submitButton");
 
 var checkMarked = document.getElementById("termosDeContrato");
