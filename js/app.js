ed35fb0d591714356749d38236bea66b52cac07c
matheusoares <matheus_desousa@live.com>
2018-04-02 06:22:30 -0300

    function terms modal login js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+
+var botao = document.getElementById("submitButton");
+
+var checkMarked = document.getElementById("termosDeContrato");
+
+checkMarked.addEventListener("click", function(){
+
+	var checkValue = document.getElementById("termosDeContrato").checked;
+	if(checkValue){
+		botao.classList.remove("disabled");
+		botao.classList.remove("avoid-clicks");
+		botao.setAttribute("aria-disabled", "false");
+	}else{
+		botao.classList.add("disabled");
+		botao.classList.add("avoid-clicks");
+	}
+});
