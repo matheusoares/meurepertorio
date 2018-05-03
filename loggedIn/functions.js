946ae0eb8d118f4d714d036b280c0881634a55e9
matheusoares <matheus_desousa@live.com>
2018-05-03 01:37:17 -0300

    init dashboard

--- loggedIn/functions.js
+++ loggedIn/functions.js
@@ -2,11 +2,24 @@ var btnSearch = document.getElementById("searchButton");
 var formSearch = document.getElementById("formSearch");
 var inputSearch = document.getElementById("inputSearch");
 var imgSearch = document.getElementById("imgSearch");
+var btnSearchSubmit = document.getElementById("btnSearchSubmit");
 
 btnSearch.addEventListener("click", function(){
 	inputSearch.focus();
+	btnSearchSubmit.classList.toggle("btnSearchSubmit");
 	formSearch.classList.toggle("hidden");
 	formSearch.classList.toggle("form-search");
+});
+
+var btnResponsiveSideNav = document.getElementById("btnResponsiveSideNav");
+var sideNav = document.getElementById("sideNav");
+var container = document.getElementById("container");
+
+btnResponsiveSideNav.addEventListener("click", function(){
+
+	sideNav.classList.toggle("off");
+	container.classList.toggle("overflow");
+
 });
 
 
