c93d2ec91d12f5cdc35c146e722bc2bb1f202bdf
matheusoares <matheus_desousa@live.com>
2018-06-22 15:46:22 -0300

    Fixed itens

--- loggedIn/assets/js/functions.js
+++ loggedIn/assets/js/functions.js
@@ -1,27 +1,19 @@
 (function(window, document, $){
 
-	// $('.search-trigger').on('click', function(){
-	//   $('.form-search').toggleClass('hidden');
-	//   $('.form-search').removeClass('init');
-	// });	
-
-
-	// var btnResponsiveSideNav = document.getElementById("btnResponsiveSideNav");
-	// var sideNav = document.getElementById("sideNav");
-	// var container = document.getElementById("container");
-
-	// btnResponsiveSideNav.addEventListener("click", function(){
-	// 	sideNav.classList.toggle("off");
-	// 	container.classList.toggle("overflow");
-
-	// });
-
 	$('.trigger, .mask').on('click', function(){
 		$('.sidenav').toggleClass('active-menu desactive-menu');
 		$('.brand').toggleClass('d-grid desactive-menu');
-		setTimeout(function(){
-			$('.mask').toggleClass('grid-mask');
-		}, 100);
+		$('.mask').toggleClass('grid-mask');
+	});
+
+	$('.search-trigger, .close-search').on('click', function(){
+		
+		if($('.off').hasClass('search')){
+			$('.off').removeClass('search').addClass('search-off');
+		}else{
+			$('.off').removeClass('search-off').addClass('search');
+		}
+		
 	});
 
 })(window, document, jQuery);
